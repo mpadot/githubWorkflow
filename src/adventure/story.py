15f14bdff20d853d2e39674a5e575179e429ca91
Miles Padot <155395178+mpadot@users.noreply.github.com@users.noreply.github.com>
2025-10-28 03:35:39 +0000

    fixed up story.py

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         choice = Prompt.ask("[bold cyan]Which direction do you choose?[/bold cyan] [bold yellow]left[/bold yellow] or [bold yellow]right[/bold yellow] or [bold red]exit[/bold red] to leave the game.")
         choice = choice.strip().lower()
         if choice == 'exit':
-            print("[bold green]Thank you for playing![/bold green]")
+            print("[bold green]Goodbye![/bold green]")
             break
         
         print(step(choice, events))
